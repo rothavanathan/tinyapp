c837afdb93c4da7febad12ad9244b4b47f36b897
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-11 23:56:07 +0000

    quick refactor with mentor for cleanliness

--- express_server.js
+++ express_server.js
@@ -39,8 +39,8 @@ const isRegisteredEmail = (email, database) => {
 
 
 const generateRandomString =  function() {
-  let result           = '';
-  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
   for (let i = 0; i < 6; i++ ) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -51,8 +51,7 @@ const generateRandomString =  function() {
 
 //root homepage
 app.get("/", (req, res) => {
-  const templateVars = 
-    { 
+  const templateVars = { 
       user: users[req.cookies["user_id"]],
       urls: urlDatabase   
     };
@@ -61,8 +60,7 @@ app.get("/", (req, res) => {
 
 //get login form 
 app.get("/login", (req, res) => {
-  const templateVars = 
-    { 
+  const templateVars = { 
       user: users[req.cookies["user_id"]], 
     };
   res.render('login', templateVars);
@@ -71,32 +69,27 @@ app.get("/login", (req, res) => {
 //login form NEEDS UPDATING TO INCLUDE PASSWORD
 app.post("/login", (req, res) => {
   const {email, password} = req.body;
-  if (!isRegisteredEmail(email, users)) {
+  const user = isRegisteredEmail(email, users)
+  if (!user) {
     return res.sendStatus(403);
-  } else {
-    const user = isRegisteredEmail(email, users)
-    if (users[user].password !== password) {
-      return res.sendStatus(403);
-    } else {
-      res.cookie("user_id", users[user].id);
-      res.redirect('/urls');
-    }
-  }
-})
+  }     
+  if (users[user].password !== password) {
+    return res.sendStatus(403);
+  } 
+  res.cookie("user_id", users[user].id);
+  res.redirect('/urls');
+});
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect('/urls')
-})
+  res.redirect('/urls');
+});
 
 
 //url index route
 app.get("/urls", (req, res) => {
-  console.log(req.cookies)
   const userId = req.cookies.user_id;
-  console.log(users[userId])
-  const templateVars = 
-  { 
+  const templateVars = { 
     user: users[userId],
     urls: urlDatabase 
   };
@@ -105,8 +98,7 @@ app.get("/urls", (req, res) => {
 
 //show new url form
 app.get("/urls/new", (req, res) => {
-  const templateVars = 
-  { 
+  const templateVars = { 
     user: users[req.cookies["user_id"]],
     urls: urlDatabase 
   };
@@ -115,18 +107,14 @@ app.get("/urls/new", (req, res) => {
 
 //posts a new url to url database
 app.post("/urls", (req, res) => {
-  console.log(req.body);// Log the POST request body to the console
-  //generate shortURL for user submitted longURL  
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL
-  res.redirect('/urls/' + shortURL);         // Respond with 'Ok' (we will replace this)
+  res.redirect('/urls/' + shortURL);
 });
 
 //show new user registration form
 app.get("/register", (req, res) => {
-  console.log(users)
-  const templateVars = 
-  { 
+  const templateVars = { 
     user: null,
     urls: urlDatabase 
   };
@@ -135,29 +123,26 @@ app.get("/register", (req, res) => {
 
 //registers a new user to database
 app.post("/register", (req, res) => {
-
   const {email, password} = req.body
   if (!email || !password) {
     return res.sendStatus(404);
-  } else if (isRegisteredEmail(email, users)) {
+  } 
+  if (isRegisteredEmail(email, users)) {
     return res.sendStatus(404).send('this email is taken');
-  } else {
-    const newUserID = generateRandomString();
-    users[newUserID] = {
-      id: newUserID,
-      email,
-      password
-    };
-    console.log(users);
-    res.cookie('user_id', newUserID);
-    res.redirect('/urls/');
-  }
+  } 
+  const newUserID = generateRandomString();
+  users[newUserID] = {
+    id: newUserID,
+    email,
+    password
+  };
+  res.cookie('user_id', newUserID);
+  res.redirect('/urls');
 });
 
 //deletes a url from url database
 app.post("/urls/:shortURL/delete", (req, res) => {
   //generate shortURL for user submitted longURL 
-  
   const shortURL = req.body.shortURL;
   //delete shortURL key from databasw
   delete urlDatabase[shortURL];
@@ -181,8 +166,7 @@ app.get("/urls/:shortURL", (req, res) => {
   if (!urlDatabase[shortURL]) {
     return res.redirect('/urls/index')  
   }
-  const templateVars = 
-  {
+  const templateVars = {
     shortURL,
     longURL: urlDatabase[shortURL],
     user: users[req.cookies["user_id"]]
